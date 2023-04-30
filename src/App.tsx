941b884020d8250e086201723581d08c81ecfcd7
Alexandra77718 <sandra.davidson777@gmail.com>
2023-04-30 20:58:15 +0300

    1_ Create_ Button_Title_Tabs_LogoIcon_add1Font

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
+import Button, {ButtonType} from './components/Button/Button';
+import { LikeIcon } from './assets/icons/LikeIcon';
+import { LogoIcon } from './assets/icons/LogoIcon';
+import UserName from './components/UserName/UserName';
+import Tabs from './components/Tabs/Tabs';
+import Title from './components/Title/Title';
 
-function App() {
+
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+              <Button title={'Primary'} type={ButtonType.Primary} onClick={()=>{}} />
+          <Button title='Disabled' type={ButtonType.Primary} onClick={()=>{}} disabled/>
+              <Button title={'Close'} type={ButtonType.Close} onClick={()=>{}}/>
+          <Button title={'Like'} type={ButtonType.Like} onClick={() => { }} />
+          <LikeIcon />
+          <UserName username='Name Surname' />
+          <Tabs />
+          <LogoIcon />
+          <Title title='New Releases Books'/>
+              
+        
     </div>
   );
 }
