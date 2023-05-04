57b425de2e14395aed9923f359ac16b2c25cc223
Alexandra77718 <sandra.davidson777@gmail.com>
2023-05-05 00:11:32 +0300

    Update Input, Header, create Subscribe, Footer

--- src/pages/PagesContainer/Footer/Footer.tsx
+++ src/pages/PagesContainer/Footer/Footer.tsx
@@ -1,9 +1,14 @@
-import React, { FC } from 'react';
-import styles from './Footer.module.scss'
+import React, { FC } from "react";
+import styles from "./Footer.module.scss";
 
 const Footer = () => {
-        return (
-        <div></div>
-    )
-}
+  return (
+    <div>
+      <div className={styles.footer}>
+        <div>©2022 Bookstore</div>
+        <div>All rights reserved</div>
+      </div>
+    </div>
+  );
+};
 export default Footer;
