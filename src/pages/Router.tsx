262f7c230439c63148e17f70388b09e4fae185a0
Alexandra77718 <sandra.davidson777@gmail.com>
2023-05-05 19:10:35 +0300

    Create ResetPassword, Confirm

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PagesContainer from "src/pages/PagesContainer/PagesContainer";
+import Home from "./Home";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
-import Home from "./Home";
+import ResetPassword from "./ResetPassword/ResetPassword";
+import Confirm from './Confirm';
 
 export enum RoutesList {
   Home = "/",
@@ -16,8 +18,9 @@ export enum RoutesList {
   SignIn = "/sign-in",
   SignUp = "/sign-up",
   Reset = "/reset",
-  Confirm = "/sign-up/confirm",
-  ChangedPassword = "/sign-up/success",
+  Confirm = "/reset/confirm",
+  NewPassword = "/reset/new-password",
+  ChangedPassword = "/sign-in/success",
 }
 
 const Router = () => {
@@ -28,10 +31,8 @@ const Router = () => {
           <Route path={RoutesList.Home} element={<Home />} />
           <Route path={RoutesList.SignIn} element={<SignIn />} />
           <Route path={RoutesList.SignUp} element={<SignUp />} />
-          {/* <Route path={RoutesList.Account} element={<Account />} />
-          <Route path={RoutesList.Reset} element={<Reset />} />
-          <Route path={RoutesList.SignUp} element={<SignUp />} />
-          <Route path={RoutesList.SignUp} element={<SignUp />} /> */}
+          <Route path={RoutesList.Reset} element={<ResetPassword />} />
+          <Route path={RoutesList.Confirm} element={<Confirm />} />
         </Route>
       </Routes>
     </BrowserRouter>
